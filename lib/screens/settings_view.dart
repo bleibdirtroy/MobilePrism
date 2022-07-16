85644bfd4120e4dd942ea343cb6b55ace77e858a
Adrian Germeck <adriangermeck7@gmail.com>
2022-07-16 18:06:03 +0200

    added license page

--- lib/screens/settings_view.dart
+++ lib/screens/settings_view.dart
@@ -6,11 +6,28 @@ class SettingsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TextButton(
-        onPressed: () {
-          Navigator.of(context).pushReplacementNamed('/login/');
-        },
-        child: const Text("Back to Login"),
+      body: Center(
+        child: Column(
+          children: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pushReplacementNamed('/login/');
+              },
+              child: const Text("Back to Login"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const LicensePage(),
+                  ),
+                );
+              },
+              child: const Text("Licenses"),
+            ),
+          ],
+        ),
       ),
     );
   }
