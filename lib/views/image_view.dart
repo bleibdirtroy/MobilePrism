0950d86e64d9d45faa7e8881e8784f8c142e9f9b
Adrian Germeck <adriangermeck7@gmail.com>
2022-07-20 17:21:11 +0200

    added image view and hide app bar

--- lib/views/image_view.dart
+++ lib/views/image_view.dart
@@ -1,14 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:mobileprism/widgets/sliding_app_bar.dart';
+import 'package:photo_view/photo_view.dart';
 
-class ImageView extends StatelessWidget {
+class ImageView extends StatefulWidget {
   const ImageView({Key? key}) : super(key: key);
+
+  @override
+  State<ImageView> createState() => _ImageViewState();
+}
+
+class _ImageViewState extends State<ImageView>
+    with SingleTickerProviderStateMixin {
+  bool _visible = true;
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: SlidingAppBar(
+        controller: _controller,
+        visible: _visible,
+        child: AppBar(),
+      ),
       body: Center(
-        child: Image.asset("assets/images/1.jpg"),
+        child: PhotoView(
+          minScale: PhotoViewComputedScale.contained,
+          maxScale: PhotoViewComputedScale.covered * 8,
+          imageProvider: const AssetImage("assets/images/large.jpg"),
+          onTapUp: (context, details, controllerValue) {
+            print("HI");
+            setState(() {
+              _visible = !_visible;
+            });
+          },
+        ),
       ),
     );
   }
---
+++ lib/widgets/sliding_app_bar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class SlidingAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const SlidingAppBar({
+    required this.child,
+    required this.controller,
+    required this.visible,
+  });
+
+  final PreferredSizeWidget child;
+  final AnimationController controller;
+  final bool visible;
+
+  @override
+  Size get preferredSize => child.preferredSize;
+
+  @override
+  Widget build(BuildContext context) {
+    visible ? controller.reverse() : controller.forward();
+    return SlideTransition(
+      position:
+          Tween<Offset>(begin: Offset.zero, end: const Offset(0, -1)).animate(
+        CurvedAnimation(parent: controller, curve: Curves.fastOutSlowIn),
+      ),
+      child: child,
+    );
+  }
+}
