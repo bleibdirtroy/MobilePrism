fa67c6080595475384eea71209cd116c6f9167f5
Adrian Germeck <adriangermeck7@gmail.com>
2022-07-20 16:33:46 +0200

    added image view and routing

--- lib/views/image_view.dart
+++ lib/views/image_view.dart
@@ -5,6 +5,11 @@ class ImageView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Ciao");
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Image.asset("assets/images/1.jpg"),
+      ),
+    );
   }
 }
