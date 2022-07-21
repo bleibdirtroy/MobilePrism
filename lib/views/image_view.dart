ccb19e1c6904596dde8ad3ab5a77317d6c01d718
Adrian Germeck <adriangermeck7@gmail.com>
2022-07-21 11:41:12 +0200

    added gallery and controller

--- lib/views/image_view.dart
+++ lib/views/image_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mobileprism/widgets/sliding_app_bar.dart';
 import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
 
 class ImageView extends StatefulWidget {
   const ImageView({Key? key}) : super(key: key);
@@ -12,11 +13,15 @@ class ImageView extends StatefulWidget {
 class _ImageViewState extends State<ImageView>
     with SingleTickerProviderStateMixin {
   bool _visible = true;
-  late final AnimationController _controller;
+  int numberOfPictures = 3;
+  late final AnimationController _animationController;
+  late final PageController _pageController;
+
 
   @override
   void initState() {
-    _controller = AnimationController(
+    _pageController = PageController(initialPage: 1);
+    _animationController = AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 300),
     );
@@ -25,7 +30,7 @@ class _ImageViewState extends State<ImageView>
 
   @override
   void dispose() {
-    _controller.dispose();
+    _animationController.dispose();
     super.dispose();
   }
 
@@ -33,21 +38,26 @@ class _ImageViewState extends State<ImageView>
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: SlidingAppBar(
-        controller: _controller,
+        controller: _animationController,
         visible: _visible,
         child: AppBar(),
       ),
-      body: Center(
-        child: PhotoView(
-          minScale: PhotoViewComputedScale.contained,
-          maxScale: PhotoViewComputedScale.covered * 8,
-          imageProvider: const AssetImage("assets/images/large.jpg"),
-          onTapUp: (context, details, controllerValue) {
-            setState(() {
-              _visible = !_visible;
-            });
-          },
-        ),
+      body: PhotoViewGallery.builder(
+        scrollPhysics: const BouncingScrollPhysics(),
+        pageController: _pageController,
+        itemCount: numberOfPictures,
+        builder: (context, index) {
+          return PhotoViewGalleryPageOptions(
+            minScale: PhotoViewComputedScale.contained,
+            maxScale: PhotoViewComputedScale.covered * 8,
+            imageProvider: const AssetImage("assets/images/large.jpg"),
+            onTapUp: (context, details, controllerValue) {
+              setState(() {
+                _visible = !_visible;
+              });
+            },
+          );
+        },
       ),
     );
   }
