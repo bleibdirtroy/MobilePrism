f5e22d71e6ddf61a17b159fd96f3cb3396fc8321
johnns-s <test@test.com>
2022-07-31 11:50:17 +0200

    Corrected flutter analyze errors

--- test/services/database_service_test.dart
+++ test/services/database_service_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mobileprism/services/database_service.dart';
+// import 'package:mobileprism/services/database_service.dart';
 
 void main() {
   testWidgets('database service ...', (tester) async {
