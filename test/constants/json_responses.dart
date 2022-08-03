518d7e7ed850eb6372c8111f1fe924f7f36c1f6b
Adrian Germeck <adriangermeck7@gmail.com>
2022-08-03 20:33:42 +0200

    added mock responses for tests

---
+++ test/constants/json_responses.dart
@@ -0,0 +1,164 @@
+const oneAlbumJson = '''
+[
+    {
+        "UID": "aqoweu7vjjr0qxzz",
+        "ParentUID": "",
+        "Thumb": "bf2a691e21b3403fc93a0e4ce2f33307cace2faf",
+        "Slug": "berlin",
+        "Type": "album",
+        "Title": "Berlin",
+        "Location": "",
+        "Category": "",
+        "Caption": "",
+        "Description": "",
+        "Notes": "",
+        "Filter": "",
+        "Order": "oldest",
+        "Template": "",
+        "Path": "",
+        "State": "",
+        "Country": "zz",
+        "Year": 0,
+        "Month": 0,
+        "Day": 0,
+        "Favorite": true,
+        "Private": false,
+        "PhotoCount": 6,
+        "LinkCount": 2,
+        "CreatedAt": "2021-02-21T21:39:43Z",
+        "UpdatedAt": "2021-02-21T21:39:43Z",
+        "DeletedAt": "0001-01-01T00:00:00Z"
+    }
+]
+''';
+
+const twoAlbumsJson = '''
+[
+    {
+        "UID": "aqoweu7vjjr0qxzz",
+        "ParentUID": "",
+        "Thumb": "bf2a691e21b3403fc93a0e4ce2f33307cace2faf",
+        "Slug": "berlin",
+        "Type": "album",
+        "Title": "Berlin",
+        "Location": "",
+        "Category": "",
+        "Caption": "",
+        "Description": "",
+        "Notes": "",
+        "Filter": "",
+        "Order": "oldest",
+        "Template": "",
+        "Path": "",
+        "State": "",
+        "Country": "zz",
+        "Year": 0,
+        "Month": 0,
+        "Day": 0,
+        "Favorite": true,
+        "Private": false,
+        "PhotoCount": 6,
+        "LinkCount": 2,
+        "CreatedAt": "2021-02-21T21:39:43Z",
+        "UpdatedAt": "2021-02-21T21:39:43Z",
+        "DeletedAt": "0001-01-01T00:00:00Z"
+    },
+    {
+        "UID": "aqowevm1jfxvr0fa",
+        "ParentUID": "",
+        "Thumb": "e4d493c81905b0267976c1e8446f9fe33bbad832",
+        "Slug": "california",
+        "Type": "album",
+        "Title": "California",
+        "Location": "",
+        "Category": "",
+        "Caption": "",
+        "Description": "",
+        "Notes": "",
+        "Filter": "",
+        "Order": "oldest",
+        "Template": "",
+        "Path": "",
+        "State": "",
+        "Country": "zz",
+        "Year": 0,
+        "Month": 0,
+        "Day": 0,
+        "Favorite": true,
+        "Private": false,
+        "PhotoCount": 6,
+        "LinkCount": 0,
+        "CreatedAt": "2021-02-21T21:40:34Z",
+        "UpdatedAt": "2021-02-21T21:40:34Z",
+        "DeletedAt": "0001-01-01T00:00:00Z"
+    }
+]
+''';
+
+const oneFolderJson = '''
+[
+    {
+        "UID": "aqoweo83c21h6eo7",
+        "ParentUID": "",
+        "Thumb": "187b0d9dd1220fd5a2b14b6f97fa83d969504d36",
+        "Slug": "demo",
+        "Type": "folder",
+        "Title": "Demo",
+        "Location": "",
+        "Category": "",
+        "Caption": "",
+        "Description": "",
+        "Notes": "",
+        "Filter": "path:demo public:true",
+        "Order": "added",
+        "Template": "",
+        "Path": "demo",
+        "State": "",
+        "Country": "zz",
+        "Year": 2020,
+        "Month": 8,
+        "Day": 31,
+        "Favorite": true,
+        "Private": false,
+        "PhotoCount": 0,
+        "LinkCount": 0,
+        "CreatedAt": "2021-02-21T21:36:08Z",
+        "UpdatedAt": "2022-08-03T18:10:08.862360735Z",
+        "DeletedAt": "0001-01-01T00:00:00Z"
+    }
+]
+''';
+
+const oneMonthJson = '''
+[
+    {
+        "UID": "aqowero39gi4lntj",
+        "ParentUID": "",
+        "Thumb": "de1aa5c5a3432a7ec3487cd0c5fdda5b022f38b5",
+        "Slug": "december-2017",
+        "Type": "month",
+        "Title": "December 2017",
+        "Location": "",
+        "Category": "",
+        "Caption": "",
+        "Description": "",
+        "Notes": "",
+        "Filter": "public:true year:2017 month:12",
+        "Order": "oldest",
+        "Template": "",
+        "Path": "",
+        "State": "",
+        "Country": "zz",
+        "Year": 2017,
+        "Month": 12,
+        "Day": 0,
+        "Favorite": true,
+        "Private": false,
+        "PhotoCount": 0,
+        "LinkCount": 0,
+        "CreatedAt": "2021-02-21T21:38:12Z",
+        "UpdatedAt": "2021-02-21T21:38:12Z",
+        "DeletedAt": "0001-01-01T00:00:00Z"
+    }
+]
+''';
