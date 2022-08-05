418fb7d64ae2d3d06ab62da2a6efc286f2e210c8
johnns-s <test@test.com>
2022-08-05 19:17:30 +0200

    First complete implementation draft of database_service.dart

--- lib/services/database/photo_data_entry.dart
+++ lib/services/database/photo_data_entry.dart
@@ -1,17 +1,19 @@
 import 'dart:ffi';
 
-class PhotoDataEntry {
-  final String uid;
-  final bool? panorama;
-  final int? width;
-  final int? height;
-  final String? imageHash;
-  final String? imageQuality;
-  final double? lat;
-  final double? long;
-  final Int64? timestamp;
+import 'package:mobileprism/services/database/database_service.dart';
 
-  const PhotoDataEntry({
+class PhotoDataEntry {
+  late final String uid;
+  late final bool? panorama;
+  late final int? width;
+  late final int? height;
+  late final String? imageHash;
+  late final String? imageQuality;
+  late final double? lat;
+  late final double? long;
+  late final Int64? timestamp;
+
+  PhotoDataEntry({
     required this.uid,
     this.panorama,
     this.width,
@@ -22,4 +24,74 @@ class PhotoDataEntry {
     this.long,
     this.timestamp,
   });
+
+  PhotoDataEntry.fromMap(Map<String, Object?> data) {
+    if (data["uid"] != null) {
+      uid = data["uid"]! as String;
+      if (data["panorama"] is int) {
+        panorama = data["panorama"]! as bool;
+      } else {
+        panorama = null;
+      }
+
+      if (data["width"] is int) {
+        width = data["width"]! as int;
+      } else {
+        width = null;
+      }
+
+      if (data["height"] is int) {
+        height = data["height"]! as int;
+      } else {
+        height = null;
+      }
+
+      if (data["image_hash"] is String) {
+        imageHash = data["image_hash"]! as String;
+      } else {
+        imageHash = null;
+      }
+
+      if (data["image_quality"] is String) {
+        imageQuality = data["image_quality"]! as String;
+      } else {
+        imageQuality = null;
+      }
+
+      if (data["lat"] is num) {
+        lat = data["lat"]! as double;
+      } else {
+        lat = null;
+      }
+
+      if (data["long"] is num) {
+        long = data["long"]! as double;
+      } else {
+        long = null;
+      }
+
+      if (data["timestamp"] is num) {
+        timestamp = data["timestamp"]! as Int64;
+      } else {
+        timestamp = null;
+      }
+    } else {
+      throw FieldsMissingException();
+    }
+  }
+
+  Map<String, dynamic> toMap(){
+    return {
+      "uid": uid,
+      "panorama": panorama,
+      "width": width,
+      "height": height,
+      "imageHash": imageHash,
+      "image_quality": imageQuality,
+      "lat": lat,
+      "long": long, 
+      "timestamp": timestamp
+    };
+  }
+
 }
