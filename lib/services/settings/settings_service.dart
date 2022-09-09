04de11b2b53d5f437754cba60168520e8e9e9878
Adrian Germeck <adriangermeck7@gmail.com>
2022-09-09 18:53:23 +0200

    extract local variable for database only

--- lib/services/settings/settings_service.dart
+++ lib/services/settings/settings_service.dart
@@ -7,18 +7,20 @@ class SettingsService {
   final _secureStorage = SecureStorageProvider();
   Future<void> setUseDatabase() async {
     await _secureStorage.upsertData(
-      "useDatabaseOnly",
+      useDatabaseOnlyKey,
       jsonEncode(PhotoPrismServer().useDatabaseOnly),
     );
   }
 
   Future<void> loadSettings() async {
-    if (await _secureStorage.existsKey("useDatabaseOnly")) {
+    if (await _secureStorage.existsKey(useDatabaseOnlyKey)) {
       PhotoPrismServer().useDatabaseOnly = jsonDecode(
-        await _secureStorage.readData("useDatabaseOnly"),
+        await _secureStorage.readData(useDatabaseOnlyKey),
       ) as bool;
     } else {
-      await _secureStorage.insertData("useDatabaseOnly", jsonEncode(false));
+      await _secureStorage.insertData(useDatabaseOnlyKey, jsonEncode(false));
     }
   }
 }
+
+const useDatabaseOnlyKey = "useDatabaseOnly";
